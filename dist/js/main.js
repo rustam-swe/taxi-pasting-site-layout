e5f71eb8523cf926e493b52ca7e90cfb84eb1d14
Rustam <rustamergashev.sp@gmail.com>
2019-09-11 17:04:14 +0300

    Мобилный экран добавлен

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,8 @@
 // Меню бургер
 const menuBtn = document.querySelector('.menu-btn'); // Кнопка меню
-const headerNav = document.querySelector('.header-nav'); // Блок с меню
-const headerMenu = document.querySelector('.header-menu'); // Меню
-const menuItems = document.querySelectorAll('.header-menu__item'); // Пункты меню
+const headerNav = document.querySelector('.nav'); // Блок с меню
+const headerMenu = document.querySelector('.nav__menu'); // Меню
+// const menuItems = document.querySelectorAll('.nav__item'); // Пункты меню
 const lightBox = document.querySelector('.light-box'); // Лайтбокс (темный фон)
 
 let showMenu = false;
@@ -23,7 +23,7 @@ function openMenu() {
   headerNav.classList.add('show');
   headerMenu.classList.add('show');
   lightBox.classList.add('show');
-  menuItems.forEach(item => item.classList.add('show'));
+  // menuItems.forEach(item => item.classList.add('show'));
   window.addEventListener('scroll', noScroll);
   showMenu = true;
 }
@@ -33,7 +33,7 @@ function closeMenu() {
   headerNav.classList.remove('show');
   headerMenu.classList.remove('show');
   lightBox.classList.remove('show');
-  menuItems.forEach(item => item.classList.remove('show'));
+  // menuItems.forEach(item => item.classList.remove('show'));
   window.removeEventListener('scroll', noScroll);
   showMenu = false;
 }
@@ -53,113 +53,4 @@ document.querySelectorAll('.callback-btn').forEach(btn => {
       behavior: 'smooth'
     })
   })
-});
-
-
-// Табы на странице новости
-const tablinks = document.querySelectorAll('.articles-tabs__item'); // Вкладки
-const tabcontent = document.querySelectorAll('.articles-content__item'); // Содержимое
-
-function switchTab(e, id) {
-
-  // let evTar = e.target;
-  // let curTar = e.currentTarget;
-  let clickedTab = e.target.attributes['id'].value;
-  let activeTabContent = document.querySelector('.articles-content__item.' + clickedTab);
-
-  // Скрытие всех данных
-  for (let i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Удаление активных классов
-  for (let i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" articles-tabs__item_active", "");
-  }
-
-  // console.log(evTar, curTar);
-
-  activeTabContent.style.display = "block";
-  e.currentTarget.className += " articles-tabs__item_active";
-}
-
-// const tablinks = document.querySelector('.articles-tabs');
-for (let i = 0; i < tablinks.length; i++) {
-  tablinks[i].addEventListener('click', switchTab);
-}
-
-// Слайдеры (Slick.js)
-$(document).ready(function () {
-
-  // Слайдер на главной странице
-  $('.home-slider').slick({
-    infinite: false,
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    responsive: [{
-      breakpoint: 1199,
-      settings: {
-        arrows: false
-      }
-    }]
-  });
-
-  // Слайдер на странице участка
-  $('.product-slider').slick({
-    infinite: false,
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    responsive: [{
-      breakpoint: 768,
-      settings: {
-        arrows: false
-      }
-    }]
-  });
-
-  // Слайдер отзывов
-  $('.testimonials__slider').slick({
-    dots: true,
-    infinite: false,
-    // centerMode: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-
-    responsive: [{
-        breakpoint: 992,
-        settings: {
-          arrows: false
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '135px',
-          slidesToShow: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          // centerPadding: '100px',
-          slidesToShow: 1
-        }
-      },
-      {
-        breakpoint: 400,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '50px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
 });
